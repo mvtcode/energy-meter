16106c13860d8e9e46107fd32953a2a11346abce
viet <zesumen123@gmail.com>
2021-04-19 14:48:53 +0700

    add

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
+const axios = require('axios');
 const SerialPort = require('serialport');
 const utils = require('./utils');
 
 const serialport = new SerialPort("COM6", {
-	baudRate: 9600,
-	autoOpen: false,
+  baudRate: 9600,
+  autoOpen: false,
 });
 
 const EL = '\r\n';
@@ -15,96 +16,195 @@ const SUB = String.fromCharCode(26);
 let seed = '';
 let step = 0;
 let result = ""
+const dataSend = {
+  time: "",
+  meterId: ""
+}
 serialport.open((err) => {
-	console.log(`Port open: ${err ? err : "ok"}`);
+  console.log(`Port open: ${err ? err : "ok"}`);
 });
 
 serialport.on('data', (data) => {
-	const string = data.toString();
-	console.log("=>", data.toString('hex'), string);
-	// step++;
-	if (step === 0) {
-		result += string
-		if (string.match('\n')) {
-			step++;
-			console.log(result)
-			result = ""
-		}
-	}
+  const string = data.toString();
+  console.log("=>", data.toString('hex'), string);
 
-	if (step === 1) {
-		if (result == "") {
-			readCommand();
-			result += " "
-		} else {
-			result += string
-			if (string.match(ETX)) {
-				step++;
-				console.log(result)
-				result = "";
-			}
-		}
-	}
-	if (step === 2) {
-		if (result == "") {
-			enterPassword();
-			result += " "
-		} else {
-			result += string
-			if (string.match(ACK)) {
-				step++;
-				result = "";
-			}
-		}
-		// enterPassword();
-	}
-	if (step === 3) {
-		readData()
-		step++;
-	}
+  if (step === 0) {
+    result += string
+    if (string.match('\n')) {
+      step++;
+      console.log(result)
+      result = ""
+    }
+  }
+
+  if (step === 1) {
+    if (result == "") {
+      readCommand();
+      result += " "
+    } else {
+      result += string
+      if (string.match(ETX)) {
+        step++;
+        console.log(result)
+        result = "";
+      }
+    }
+  }
+
+  if (step === 2) {
+    if (result == "") {
+      enterPassword();
+      result += " "
+    } else {
+      result += string
+      if (string.match(ACK)) {
+        step++;
+        result = "";
+      }
+    }
+  }
+
+  // meter ID
+  if (step === 3) {
+    if (result == "") {
+      readMeterId();
+      result += " "
+    } else {
+      result += string
+      if (string.match(ETX)) {
+        console.log("Id:", utils.getId(result));
+        dataSend.meterId = utils.getId(result)
+        step++;
+        result = ""
+      }
+    }
+  }
+
+  //date time
+  if (step === 4) {
+    if (result == "") {
+      readDataTime();
+      result += " "
+    } else {
+      result += string
+      if (string.match(ETX)) {
+        console.log(result);
+        console.log("Time: ", utils.getTimeFormMess(result));
+        dataSend.time = utils.getTimeFormMess(result)
+        step++;
+        result = ""
+        axios.post('http://localhost:9000/meter', {
+          ...dataSend
+        })
+          .then(function (response) {
+            // handle success
+            console.log("Send Success",per);
+          })
+          .catch(function (error) {
+            // handle error
+            console.log(error.message);
+          })
+          .then(function () {
+            // always executed
+          });
+      }
+    }
+  }
+
+
+  //energy 
+  // if (step === 3) {
+  // 	if (result == "") {
+  // 		readDataMeter();
+  // 		result += " "
+  // 	} else {
+  // 		result += string
+  // 		if (string.match(ETX)) {
+  // 			console.log(result);
+  // 			console.log("Energy: ",utils.getDataMeter(result));
+  // 			step++;
+  // 			result=""
+  // 		}
+  // 	}
+  // }
 });
 
 serialport.on('open', () => {
-	wakeupCommand();
+  wakeupCommand();
 });
 
 const wakeupCommand = () => {
-	serialport.write(Buffer.from(`/?!${EL}`), err => {
-		if (err) console.err(err);
-		else console.log("wakeup:");
-	});
+  serialport.write(Buffer.from(`/?!${EL}`), err => {
+    if (err) console.err(err);
+    else console.log("wakeup:");
+  });
 };
 
 const readCommand = () => {
-	const passwordHEX = utils.string2Hex(utils.toHex("051")).toUpperCase()
-	console.log(passwordHEX);
-	serialport.write(Buffer.from(`${ACK}051${EL}`), err => {
-		if (err) console.err(err);
-		else console.log("Read:");
-	});
+  const passwordHEX = utils.string2Hex(utils.toHex("051")).toUpperCase()
+  console.log(passwordHEX);
+  serialport.write(Buffer.from(`${ACK}051${EL}`), err => {
+    if (err) console.err(err);
+    else console.log("Read:");
+  });
 };
 
 const enterPassword = () => {
-	console.log('P:', "M_KH_DOC");
-	const BCC =utils.bcc(`P2${STX}(M_KH_DOC)${ETX}`)
-	const command =`${SOH}P2${STX}(M_KH_DOC)${ETX}${BCC}`
-	serialport.write(command, err => {
-		if (err) console.err(err);
-		else console.log("Password: ",command);
-	});
+  console.log('P:', "M_KH_DOC");
+  const BCC = utils.bcc(`P2${STX}(M_KH_DOC)${ETX}`)
+  const command = `${SOH}P2${STX}(M_KH_DOC)${ETX}${BCC}`
+  serialport.write(command, err => {
+    if (err) console.err(err);
+    else console.log("Password: ", command);
+  });
 };
 
-const readData = () => {
-	const BCC =utils.bcc(`R1${STX}2(00000000)${ETX}`)
-	const command = `${SOH}R1${STX}2(00000000)${ETX}${BCC}`
-	serialport.write(command, err => {
-		if (err) console.err(err);
-		else console.log(`Read Data 1: ==> `,command);
-	});
+const readDataTime = () => {
+  const BCC = utils.bcc(`R1${STX}1(00000000)${ETX}`)
+  const command = `${SOH}R1${STX}1(00000000)${ETX}${BCC}`
+  serialport.write(command, err => {
+    if (err) console.err(err);
+    else console.log(`Read Data Time: ==> `, command);
+  });
+};
+
+const readMeterId = () => {
+  const BCC = utils.bcc(`R1${STX}0(00000000)${ETX}`)
+  const command = `${SOH}R1${STX}0(00000000)${ETX}${BCC}`
+  serialport.write(command, err => {
+    if (err) console.err(err);
+    else console.log(`Read Data Id meter: ==> `, command);
+  });
+};
+const readDataEnergy = () => {
+  const BCC = utils.bcc(`R1${STX}6(00010000)${ETX}`)
+  const command = `${SOH}R1${STX}6(00010000)${ETX}${BCC}`
+  serialport.write(command, err => {
+    if (err) console.err(err);
+    else console.log(`Read Data Meter: ==> `, command);
+  });
+};
+
+const readDataEnergy2 = () => {
+  const BCC = utils.bcc(`R1${STX}6(00010000)${ETX}`)
+  const command = `${SOH}R1${STX}6(00010000)${ETX}${BCC}`
+  serialport.write(command, err => {
+    if (err) console.err(err);
+    else console.log(`Read Data Meter: ==> `, command);
+  });
+};
+
+const readDataMeter = () => {
+  const BCC = utils.bcc(`R1${STX}<(03000000)${ETX}`)
+  const command = `${SOH}R1${STX}<(03000000)${ETX}${BCC}`
+  serialport.write(command, err => {
+    if (err) console.err(err);
+    else console.log(`Read Data Meter: ==> `, command);
+  });
 };
 
 process.on('SIGINT', function () {
-	serialport.close((err) => {
-		console.log('close', err);
-	});
+  serialport.close((err) => {
+    console.log('close', err);
+  });
 });
--- utils.js
+++ utils.js
@@ -1,46 +1,105 @@
 const HexToBytes = hex => {
-	const arr = [];
-	for(let i = 0; i < hex.length / 2; i++) {
-		arr.push(parseInt(hex.substring(i * 2, i * 2 + 2), 16));
-	}
-	return arr;
+  const arr = [];
+  for (let i = 0; i < hex.length / 2; i++) {
+    arr.push(parseInt(hex.substring(i * 2, i * 2 + 2), 16));
+  }
+  return arr;
 };
 
 module.exports.string2Hex = s => {
-	let hex = '';
-	for(let i = 0; i < s.length; i++) {
-		hex += s.charCodeAt(i).toString(16);
-	}
-	return hex;
+  let hex = '';
+  for (let i = 0; i < s.length; i++) {
+    hex += s.charCodeAt(i).toString(16);
+  }
+  return hex;
 };
 
 module.exports.toHex = arr => {
-	let hex = '';
-	for(let i = 0; i < arr.length; i++) {
-		hex += arr[i].toString(16);
-	}
-	return hex;
+  let hex = '';
+  for (let i = 0; i < arr.length; i++) {
+    hex += arr[i].toString(16);
+  }
+  return hex;
 }
 
 module.exports.elsterEncrypt = (password, seed) => {
-	const s = HexToBytes(seed);
-	const crypted = [];
-	for(let i = 0; i < 8; i++) {
-		crypted.push(password.charCodeAt(i) ^ s[i]);
-	}
-	let last = crypted[7];
-	for(let i = 0; i < 8; i++) {
-		crypted[i] = (crypted[i] + last) & 0xFF;
-		last = crypted[i];
-	}
+  const s = HexToBytes(seed);
+  const crypted = [];
+  for (let i = 0; i < 8; i++) {
+    crypted.push(password.charCodeAt(i) ^ s[i]);
+  }
+  let last = crypted[7];
+  for (let i = 0; i < 8; i++) {
+    crypted[i] = (crypted[i] + last) & 0xFF;
+    last = crypted[i];
+  }
 
-	return string2Hex(toHex(crypted)).toUpperCase();
+  return string2Hex(toHex(crypted)).toUpperCase();
 }
 
-module.exports.bcc=(s)=>{
-	let result =  s.charCodeAt(0)
-	for(let i = 1; i < s.length; i++) {
-		result =  result ^s.charCodeAt(i)
-	}
-	return String.fromCharCode(result)
+module.exports.bcc = (s) => {
+  let result = s.charCodeAt(0)
+  for (let i = 1; i < s.length; i++) {
+    result = result ^ s.charCodeAt(i)
+  }
+  return String.fromCharCode(result)
+}
+
+const hex2Dec = (s) => {
+  return parseInt(s, 16);
+}
+
+const getDataFormMess = (result) => {
+  return result.split("(")[1].split(")")[0]
+}
+const reverseString = (s) => {
+  const result = s.split("").reverse().join("");
+  return result
+}
+
+module.exports.getTimeFormMess = (result) => {
+	const dataByte = getDataFormMess(result)
+	const timeData = HexToBytes(dataByte)
+	const length = timeData.length
+	const time = new Date(`20${timeData[length - 1]}`, timeData[length - 2] - 1, timeData[length - 3], timeData[length - 4], timeData[length - 5], timeData[length - 6])
+	return time
+}
+
+module.exports.getId = (result) => {
+	const dataByte = getDataFormMess(result)
+	const reverseData = dataByte.match(/..?/g).reverse().join("");
+	const idData = hex2Dec(reverseData)
+	return idData
+}
+
+module.exports.getDataEnergy = (result) => {
+  const dataByte = getDataFormMess(result)
+  console.log("fulldata",dataByte);
+  const perData = dataByte.match(/........?/g)
+  console.log(perData);
+  const lengthPer = perData.length
+  const kq = []
+  for (let i = 0; i < lengthPer; i++) {
+    kq.push(hex2Dec(perData[i]))
+  }
+  console.log(kq);
+  // const data = HexToBytes(perData)
+  return kq
+}
+
+module.exports.getDataMeter = (result) => {
+  const dataByte = getDataFormMess(result)
+	const timeData = HexToBytes(dataByte)
+  console.log(timeData);
+  console.log("fulldata",dataByte);
+  const perData = dataByte.match(/........?/g)
+  console.log(perData);
+  const lengthPer = perData.length
+  const kq = []
+  for (let i = 0; i < lengthPer; i++) {
+    kq.push(hex2Dec(perData[i]))
+  }
+  console.log(kq);
+  // const data = HexToBytes(perData)
+  return kq
 }
